df9cd523323de245aa8a070b6b12646155f9fde1
Dennis <d.wildmark@gmail.com>
2016-06-14 10:22:03 +0200

    Added JS functionality

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ $(document).ready(function() {
     id: undefined,
     name: undefined,
     address: undefined,
-    images: [],
+    images: {
+      today: undefined,
+      future: undefined,
+      history: undefined
+    },
     story: {
       today: undefined,
       future: undefined,
@@ -24,16 +28,29 @@ $(document).ready(function() {
   $.get('http://build.dia.mah.se/pois/' + id, function(data, status) {
     building.id = data['results'][0]['id'];
     building.name = data['results'][0]['extras']['info'].name;
-    addBuildingData();
+    building.address = data['results'][0]['extras']['info']['address'].full;
   });
 
   //GET POIS STORY
   $.get('http://build.dia.mah.se/ugc/' + id + '/stories', function(data, status) {
+    building.images.today = data['stories'][0]['today'].image;
+    building.images.future = data['stories'][0]['future'].image;
+    building.images.history = data['stories'][0]['history'].image;
+    building.story.today = data['stories'][0]['today'].story;
+    building.story.future = data['stories'][0]['future'].story;
+    building.story.history = data['stories'][0]['history'].story;
     //console.log(data);
+    addBuildingData();
   });
 
   function addBuildingData() {
     $('#title').html(building.name);
+    $('#img-today').attr('src',building.images.today);
+    $('#img-future').attr('src',building.images.future);
+    $('#img-history').attr('src',building.images.history);
+    $('#future').html(building.story.future);
+    $('#history').html(building.story.history);
+    $('#today').html(building.story.today);
   }
 
 
