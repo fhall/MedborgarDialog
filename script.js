c2246ae1d231822e22dd8fd5c43260389aa4b429
John George <bravelygeorge@gmail.com>
2016-06-14 09:45:05 +0200

    data can now be inserted to the DOM

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 $(document).ready(function() {
   //Function to get url params
   $.urlParam = function(name) {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-    return results[1] || 0;
-  }
-
-  //Save get url param id to var id
+      var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+      return results[1] || 0;
+    }
+    //Save get url param id to var id
   var id = $.urlParam('id');
+
+  //building object
+  var building = {
+    id: undefined,
+    name: undefined,
+    address: undefined,
+    images: [],
+    story: {
+      today: undefined,
+      future: undefined,
+      history: undefined
+    }
+  }
 
   //POIS GET REQUEST
   $.get('http://build.dia.mah.se/pois/' + id, function(data, status) {
-    var id = data['results'][0]['id'];
-    console.log('id från get request är ' + id);
+    building.id = data['results'][0]['id'];
+    building.name = data['results'][0]['extras']['info'].name;
+    addBuildingData();
   });
 
   //GET POIS STORY
   $.get('http://build.dia.mah.se/ugc/' + id + '/stories', function(data, status) {
-    console.log(data);
+    //console.log(data);
   });
 
-
+  function addBuildingData() {
+    $('#title').html(building.name);
+  }
 
 
 
