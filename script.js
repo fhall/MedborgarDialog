7aa66d5e97c82d6d06c415a039b0788124381923
John George <bravelygeorge@gmail.com>
2016-06-14 08:16:20 +0200

    some more javascript fixes

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
   //POIS GET REQUEST
   $.get("http://build.dia.mah.se/pois/00e1012", function(data, status) {
-    //alert("Data: " + data + "\nStatus: " + status);
+    var id = data['results'][0]['id'];
+    console.log('id från get request är ' + id);
+  });
+
+
+  /*
+  //UGC POST THUMBS UP OR THUMBS DOWN
+  $.post("http://build.dia.mah.se/ugc/:id/:key/:index", {
+    id: undefined, //NOTE String
+    key: undefined, //NOTE String optional
+    index: undefined //NOTE String optional
+  }, function(data, status) {
     console.log(data);
     console.log(status);
-  });
+  });*/
 
 });
