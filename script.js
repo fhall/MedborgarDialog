04d8e8caf199ce00f3168aaaa5bbd100a2eab9e8
John George <bravelygeorge@gmail.com>
2016-06-14 19:51:26 +0200

    fixed javascript for cleaner syntax and comments  and latest ES6 standards

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     //Save get url param id to var id
   var id = $.urlParam('id');
 
-  //building object
+  //Building object built out of GET response data
   var building = {
     id: undefined,
     name: undefined,
@@ -24,26 +24,8 @@ $(document).ready(function() {
     }
   }
 
-  //POIS GET REQUEST
-  $.get('http://build.dia.mah.se/pois/' + id, function(data, status) {
-    building.id = data['results'][0]['id'];
-    building.name = data['results'][0]['extras']['info'].name;
-    building.address = data['results'][0]['extras']['info']['address'].full;
-  });
-
-  //GET POIS STORY
-  $.get('http://build.dia.mah.se/ugc/' + id + '/stories', function(data, status) {
-    building.images.today = data['stories'][0]['today'].image;
-    building.images.future = data['stories'][0]['future'].image;
-    building.images.history = data['stories'][0]['history'].image;
-    building.story.today = data['stories'][0]['today'].story;
-    building.story.future = data['stories'][0]['future'].story;
-    building.story.history = data['stories'][0]['history'].story;
-    //console.log(data);
-    addBuildingData();
-  });
-
-  function addBuildingData() {
+  //Function to append data to html elements
+  var addBuildingData = () => {
     $('#title').html(building.name);
     $('#img-today').attr('src', building.images.today);
     $('#img-future').attr('src', building.images.future);
@@ -54,7 +36,30 @@ $(document).ready(function() {
     $('#name').html(building.name + '.');
   }
 
+  //Function to GET all data by making two get requests and applying it to building object
+  var getData = () => {
+    //GET position of interest building info
+    $.get('http://build.dia.mah.se/pois/' + id, function(data, status) {
+      building.id = data['results'][0]['id'];
+      building.name = data['results'][0]['extras']['info'].name;
+      building.address = data['results'][0]['extras']['info']['address'].full;
+    });
 
+    //GET building story data
+    $.get('http://build.dia.mah.se/ugc/' + id + '/stories', function(data, status) {
+      building.images.today = data['stories'][0]['today'].image;
+      building.images.future = data['stories'][0]['future'].image;
+      building.images.history = data['stories'][0]['history'].image;
+      building.story.today = data['stories'][0]['today'].story;
+      building.story.future = data['stories'][0]['future'].story;
+      building.story.history = data['stories'][0]['history'].story;
+      //Add response data to building object
+      addBuildingData();
+    });
+  }
+
+  //Get data and append it to bulding object
+  getData();
 
   /*
   //UGC POST THUMBS UP OR THUMBS DOWN
