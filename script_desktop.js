1d182d03d10cfd10b22471262dd7f41fc041fe06
Dennis <d.wildmark@gmail.com>
2016-06-15 17:11:35 +0200

    desktop app created

---
+++ script_desktop.js
@@ -0,0 +1,93 @@
+/* <div class="row col s3">
+  <div class="card">
+    <div class="card-content">
+      <span class="card-title">Card Title</span>
+      <p>I am a very simple card. I am good at containing small bits of information.
+      I am convenient because I require little markup to use effectively.</p>
+    </div>
+    <div class="card-action">
+      <a href="#">This is a link</a>
+      <a href="#">This is a link</a>
+    </div>
+  </div>
+</div>
+*/
+$(document).ready(function() {
+  var presentCard = function(id) {
+    var building = {
+      id: undefined,
+      name: undefined,
+      address: undefined,
+      images: {
+        today: undefined,
+        future: undefined,
+        history: undefined
+      },
+      story: {
+        today: undefined,
+        future: undefined,
+        history: undefined
+      },
+      comments: [
+        {
+          title: undefined,
+          name: undefined,
+          comment: undefined
+        }
+      ]
+    }
+
+    var getFromPoi = function() {
+      return $.get('http://build.dia.mah.se/pois/' + id, function(data, status) {
+        building.id = data['results'][0].id;
+        building.name = data['results'][0].name;
+        building.address = data['results'][0]['extras']['info']['address'].full;
+      });
+    }
+
+    var getFromUgc = function() {
+      return $.get('http://build.dia.mah.se/ugc/' + id + '/stories', function(data, status) {
+        building.images.today = data['stories'][0]['today'].image;
+        building.images.future = data['stories'][0]['future'].image;
+        building.images.history = data['stories'][0]['history'].image;
+        building.story.today = data['stories'][0]['today'].story;
+        building.story.future = data['stories'][0]['future'].story;
+        building.story.history = data['stories'][0]['history'].story;
+      });
+    }
+
+    var addCard = function() {
+      $('#main-row').append('<div class="row col s3">'+
+                            '<div class="card medium">'+
+                            '<div class="card-image">'+
+                            '<img src="' + building.images.future + '">'+
+                            '</div>'+
+                            '<div class="card-content">'+
+                            '<span class="card-title">' + building.name +'</span>'+
+                            '<p>' + building.story.today + '</p>'+
+                            '</div>'+
+                            '<div class="card-action">'+
+                            '<a class="waves-effect waves-light btn modal-trigger" href="#' + building.id + '">Läs mer</a>'+
+                            '</div>'+
+                            '</div>'+
+                            '</div>');
+      $('#main').append('<div id="' + building.id + '" class="modal">'+
+                        '<div class="modal-content">'+
+                        '<h4>' + building.name + '</h4>'+
+                        '<p>' + 'Comments' +'</p>'+
+                        '</div>'+
+                        '<div class="modal-footer">'+
+                        '<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Agree</a>'+
+                        '</div>'+
+                        '</div>')
+      $('.modal-trigger').leanModal();
+    }
+
+    $.when(getFromPoi(), getFromUgc()).then(addCard);
+  }
+  presentCard("01749");
+  presentCard("01750");
+  presentCard("01751");
+  presentCard("01752");
+  presentCard("01753");
+});
