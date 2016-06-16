632fd0c7d6adb70c3b240700a7fa895b422ebc6a
Dennis <d.wildmark@gmail.com>
2016-06-16 12:32:16 +0200

    Modal panel v0.1 done

--- script_desktop.js
+++ script_desktop.js
@@ -23,6 +23,8 @@ $(document).ready(function() {
       ]
     }
 
+    var downVoteWidth = "";
+
     var getFromPoi = function() {
       return $.get('http://build.dia.mah.se/pois/' + id, function(data) {
         building.id = data['results'][0].id;
@@ -42,6 +44,18 @@ $(document).ready(function() {
       });
     }
 
+    var getCurrentVoteStatus = function() {
+      return $.get('http://build.dia.mah.se/ugc/' + id + '/votes/0', function(response, status) {
+        if (status === 'success') {
+          var downVotesTotal = response['votes'][0]['down'];
+          var upVotesTotal = response['votes'][0]['up'];
+          var totalVotes = parseInt(downVotesTotal) + parseInt(upVotesTotal);
+          downVoteWidth = (Math.round((downVotesTotal / totalVotes) * 100)).toString() + '%';
+        }
+      });
+    }
+
+
     var addCard = function() {
       $('#main-row').append('<div class="row col s3">' +
         '<div class="card medium">' +
@@ -50,13 +64,13 @@ $(document).ready(function() {
         '</div>' +
         '<div class="card-content">' +
         '<span class="card-title">' + building.name + '</span>' +
-        '<p>' + building.story.today + '</p>' +
         '</div>' +
         '<div class="card-action">' +
         '<a class="waves-effect waves-light btn modal-trigger" href="#' + building.id + '">Läs mer</a>' +
         '</div>' +
         '</div>' +
         '</div>');
+      /*
       $('#main').append('<div id="' + building.id + '" class="modal">' +
         '<div class="modal-content">' +
         '<h4>' + building.name + '</h4>' +
@@ -66,10 +80,63 @@ $(document).ready(function() {
         '<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>' +
         '</div>' +
         '</div>')
+        */
+      $('#main').append('<div id="' + building.id + '" class="modal">'+
+        '<div class="modal-content">'+
+          '<h4>' + building.name + '</h4>'+
+          '<div class="center-align">'+
+            '<div class="progress">'+
+              '<div class="determinate" style="width:' + downVoteWidth + '">'+
+              '</div>'+
+            '</div>'+
+          '</div>'+
+          '<h5>Vad ska vi utöka platsen med?</h5>'+
+          '<table class="highlight">'+
+            '<thead>'+
+              '<tr>'+
+                  '<th data-field="id">Förslag</th>'+
+                  '<th data-field="votes">Röster</th>'+
+                  '<th data-field="percent">%</th>'+
+              '</tr>'+
+            '</thead>'+
+
+            '<tbody>'+
+              '<tr>'+
+                '<td>Soptunnor</td>'+
+                '<td>10</td>'+
+                '<td>62.5%</td>'+
+              '</tr>'+
+              '<tr>'+
+                '<td>Lekplats</td>'+
+                '<td>5</td>'+
+                '<td>31.25%</td>'+
+              '</tr>'+
+              '<tr>'+
+                '<td>Toaletter</td>'+
+                '<td>1</td>'+
+                '<td>6.25%</td>'+
+              '</tr>'+
+            '</tbody>'+
+          '</table>'+
+
+          '<div class="row valign-wrapper center-align">'+
+            '<div class="input-field col s10">'+
+              '<input placeholder="Lägg till förslag" id="first_name" type="text" class="validate">'+
+            '</div>'+
+            '<div class="col s2">'+
+              '<a class="btn-floating btn-large waves-effect waves-light teal"><i class="material-icons">add</i></a>'+
+            '</div>'+
+          '</div>'+
+
+        '</div>'+
+        '<div class="modal-footer">'+
+          '<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Stäng</a>'+
+        '</div>'+
+      '</div>')
       $('.modal-trigger').leanModal();
     }
 
-    $.when(getFromPoi(), getFromUgc())
+    $.when(getFromPoi(), getFromUgc(), getCurrentVoteStatus())
       .then(addCard, function() {
         throw new Error('Something went wrong with your request');
       });
